aced91654b17eae0fcd64e55dbc9817cf6daaa03
paigexc70 <catherinep1090@gmail.com>
2023-03-02 23:40:34 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,17 +1,12 @@
 import pygame, sys
 from pygame.locals import *
 
-sound_effect = ('cheer.wav')
-
 pygame.display.set_caption('Project')
 
-GREEN = (0, 128, 0)
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
-GREY = (50, 50, 50)
 YELLOW = (255, 255, 0)
 DARKGREEN = (0, 100, 0)
-LIGHTGREY = (100, 100, 100)
 BGCOLOR = DARKGREEN
 
 GRID_SIZE = 10
